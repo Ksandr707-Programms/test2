aa77b3ce65de680a97c015ed6008f7615c14d905
Ksandr707-Programms <105298159+Ksandr707-Programms@users.noreply.github.com>
2022-05-17 17:19:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -164,7 +164,7 @@ function startGame(){
 }
 function check(){
     ostatok--;
-     ost_time.innerHTML="+ostatok+ seconds left..."
+     ost_time.innerHTML="seconds left:" "+ostatok+" 
      if(ostatok<=0){
         window.open("lose.htm")
         window.close()
